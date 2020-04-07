48a6f48d8c2be712ad167fdb19fb73c39cb2120d
AGTester <a.gad.tester@gmail.com>
2020-04-07 16:40:45 +0200

    All

---
+++ src/main/java/Pages/DonePage.java
@@ -0,0 +1,20 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class DonePage {
+    private WebDriver driver;
+    private By statusAlert = By.id("bookings");
+
+
+
+    public DonePage(WebDriver driver){ this.driver = driver; }
+
+    public String getAlertText(){
+        return driver.findElement(statusAlert).getText();
+    }
+
+
+
+}
